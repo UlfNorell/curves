b66f8bfc6d85e9f859591662ebd21ff5de1f8bc2
Ulf Norell <ulf.norell@gmail.com>
2014-01-30 17:58:33 +0100

    library for example html generation

---
+++ examples/ExampleGen.hs
@@ -0,0 +1,51 @@
+
+module ExampleGen where
+
+import Control.Applicative
+import Control.Exception
+import System.IO
+import System.Directory
+import Graphics.Curves
+
+makeImage name w h i = makeImage' name w h (autoFit 0 (Vec (fromIntegral w) (fromIntegral h)) i)
+
+makeImage' name w h i = do
+  hPutStr stderr $ "Rendering " ++ name ++ "... "
+  renderImage ("images/" ++ name ++ ".png") w h white i
+  hPutStrLn stderr "done"
+  putStr $ "<img src=\"../images/" ++ name ++ ".png\"/>"
+
+exampleHeader =
+  putStr "<head><link href='http://fonts.googleapis.com/css?family=Open+Sans|Droid+Sans+Mono&subset=latin,greek-ext' rel='stylesheet' type='text/css'></head>"
+
+getFootnotes :: IO [(String, Int)]
+getFootnotes = do
+  exist <- doesFileExist ".footnotes"
+  if not exist then return []
+               else do
+      x <- read <$> readFile ".footnotes"
+      x `seq` return x
+
+setFootnotes :: [(String, Int)] -> IO ()
+setFootnotes notes = writeFile ".footnotes" (show notes)
+
+footnote s = do
+  notes <- getFootnotes
+  let i = 1 + maximum (0 : map snd notes)
+  setFootnotes $ (s, i) : notes
+  putStrLn $ "<a name=\"" ++ s ++ "-back\"/><a class=footnote href=\"#" ++ s ++ "\">" ++ show i ++ "</a>"
+
+footnoteDef s = do
+  notes <- getFootnotes
+  let i = maybe "?" show $ lookup s notes
+  putStr $ unlines
+    [ "<p><a name=" ++ show s ++ "/>"
+    , "<a class=footnote href=\"#" ++ s ++"-back\">▲ " ++ i ++ "</a>" ]
+
+catchIO :: IO a -> (IOException -> IO a) -> IO a
+catchIO = catch
+
+done = do
+  removeFile ".footnotes" `catchIO` \_ -> return ()
+  hPutStrLn stderr "Done"
+
