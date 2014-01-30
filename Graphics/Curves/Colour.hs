b2145a76332238e359bb6e3e260e2f4c19d0b4e9
Ulf Norell <ulf.norell@gmail.com>
2014-01-30 16:13:05 +0100

    generalise Colour into a functor

--- Graphics/Curves/Colour.hs
+++ Graphics/Curves/Colour.hs
@@ -1,17 +1,40 @@
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 {-| RGBA colour values.
 -}
 module Graphics.Curves.Colour where
 
+import Control.Applicative
+import Data.Foldable hiding (foldr)
+import Data.Traversable
+
 import Graphics.Curves.Math
 
 -- | RGBA values in the range 0.0 to 1.0.
-data Colour = Colour { getRed, getGreen, getBlue, getAlpha :: !Scalar }
-  deriving (Eq, Ord)
+type Colour = Colour' Scalar
 
-instance Show Colour where
+-- | RGBA values parameterised on the colour value type.
+data Colour' a = Colour { getRed, getGreen, getBlue, getAlpha :: !a }
+  deriving (Eq, Ord, Functor, Foldable, Traversable)
+
+instance Applicative Colour' where
+  pure x = Colour x x x x
+  Colour fr fg fb fa <*> Colour r g b a = Colour (fr r) (fg g) (fb b) (fa a)
+
+instance Show a => Show (Colour' a) where
   showsPrec p (Colour r g b a) =
     showParen (p > 9) $
       showString "Colour" . foldr (\x f -> showString " " . shows x . f) id [r, g, b, a]
+
+truncColour :: (Ord a, Num a) => Colour' a -> Colour' a
+truncColour = fmap (max 0 . min 1)
+
+instance (Ord a, Num a) => Num (Colour' a) where
+  a + b       = truncColour $ (+) <$> a <*> b
+  a - b       = truncColour $ (-) <$> a <*> b
+  a * b       = truncColour $ (*) <$> a <*> b
+  abs         = fmap abs
+  signum      = fmap signum
+  fromInteger = pure . fromInteger
 
 -- | > opacity a c = setAlpha (a * getAlpha c) c
 opacity :: Scalar -> Colour -> Colour
